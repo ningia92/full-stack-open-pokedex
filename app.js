a3c6982cc83f1f2f291e07ac9a8268e759942d20
ningia <113545072+ningia92@users.noreply.github.com>
2025-06-05 13:43:31 +0200

    Fix code for correct deployment

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3')
+  res.send('4')
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw ('error...  ')
   res.send('ok')
 })
 
